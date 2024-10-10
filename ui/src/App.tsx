4f86ea9d0fa49640bf940bab4259b93970d8fb6d
olisa <olisaobinna@gmail.com>
2024-10-10 00:52:31 -0500

    add build folder for demo

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,13 +1,8 @@
 // src/App.tsx
-import React, {useEffect, useContext} from 'react';
+import React, {useEffect} from 'react';
 import {Routes, Route, useLocation} from 'react-router-dom';
-import {CssBaseline, Container} from '@mui/material';
-import Register from './pages/Register';
-import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import Dashboard2 from './pages/Dashboard2';
-import Navbar from './components/Navbar';
-import {AuthContext} from './services/AuthContext';
 import {Authenticator} from "@aws-amplify/ui-react";
 import '@aws-amplify/ui-react/styles.css';
 import { AuthUser } from 'aws-amplify/auth';
@@ -15,19 +10,9 @@ import { AuthUser } from 'aws-amplify/auth';
 
 const App: React.FC = () => {
     const location = useLocation();
-    const auth = useContext(AuthContext);
-
     useEffect(() => {
         if (location.pathname === '/callback') {
-            // Auth.handleRedirectPromise()
-            //     .then((user) => {
-            //         if (user) {
-            //             // User successfully signed in
-            //         }
-            //     })
-            //     .catch((err) => {
-            //         console.error(err);
-            //     });
+
         }
     }, [location]);
 
@@ -36,9 +21,6 @@ const App: React.FC = () => {
         <Authenticator>
             {({signOut, user}) => (
                 <>
-                    {/*<CssBaseline/>*/}
-                    {/*<Navbar onSignOut={signOut} currentUser={user as AuthUser} />*/}
-                    {/*<Container >*/}
                         <Routes>
                             <Route path="/" element={<Dashboard currentUser={user as AuthUser} />}/>
                             {/*<Route path="/register" element={<Register/>}/>*/}
@@ -47,7 +29,7 @@ const App: React.FC = () => {
                             {/* Add a callback route if using Hosted UI */}
                             <Route path="/callback" element={<div>Loading...</div>}/>
                         </Routes>
-                    {/*</Container>*/}
+
                 </>
             )}
         </Authenticator>
